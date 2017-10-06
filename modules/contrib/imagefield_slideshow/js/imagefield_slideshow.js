7ac92839d2dff4db12af04ccc49d5518d7e40fe5
Tiger Wing-Lawrence <tiger.wing-lawrence@tiger-wing-lawrence-laptop.local>
2017-10-06 13:23:19 -0700

    SDHT-25 Adding image slideshow for device detail page

---
+++ modules/contrib/imagefield_slideshow/js/imagefield_slideshow.js
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * Defines Javascript behaviors for the imagefield_slideshow module.
+ */
+
+(function ($, Drupal, drupalSettings) {
+
+  'use strict';
+
+  $(document).ready(function () {
+
+    var slideshow = jQuery('.imagefield_slideshow').cycle({
+      fx: drupalSettings.imagefield_slideshow.effect,
+      pause: drupalSettings.imagefield_slideshow.pause,
+      speed: drupalSettings.imagefield_slideshow.speed
+    });
+
+    // If prev/next setting is enabled.
+    if (drupalSettings.imagefield_slideshow.prev_next) {
+      jQuery('.imagefield_slideshow-prev').click(function() {
+        slideshow.cycle('prev');
+      });
+      jQuery('.imagefield_slideshow-next').click(function() {
+        slideshow.cycle('next');
+      });
+    }
+
+  });
+
+})(jQuery, Drupal, drupalSettings);
---
+++ modules/contrib/imagefield_slideshow/vendor/heykarthikwithu/cycle/README.md
@@ -0,0 +1,2 @@
+# cycle
+jQuery Cycle Plugin
